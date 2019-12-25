68f3595512098b03b6950529dd321531e033bdea
dylan <147272828@qq.com>
2019-12-25 16:45:22 +0800

    1232131

--- src/org/westos/demo/MyTest.java
+++ src/org/westos/demo/MyTest.java
@@ -3,5 +3,7 @@ package org.westos.demo;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("abc");
+        System.out.println("sdkuewrewuir");
+        System.out.println(213213213);
     }
 }
