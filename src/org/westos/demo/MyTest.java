de2d74e36d226bfff5af4f3e607df1ef879d8434
dylan <147272828@qq.com>
2019-12-25 17:27:29 +0800

    aSADAD

--- src/org/westos/demo/MyTest.java
+++ src/org/westos/demo/MyTest.java
@@ -3,5 +3,8 @@ package org.westos.demo;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("abc");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
